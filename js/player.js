f8667db47f6c71297a23cbb457ca0f0ee61a6972
Ayaan925 <anhita.ramodiya@gmail.com>
2020-12-05 09:02:10 -0500

    minor changes

--- js/player.js
+++ js/player.js
@@ -1,11 +1,12 @@
 class Player{
 constructor(){
-    this.animation = loadAnimation("../images/harry0.png","../images/harry1.png" );
-      this.sprite = createSprite(800,600);
+    this.animation = loadAnimation("./images/harry0.png","./images/harry1.png" );
+      this.sprite = createSprite(windowWidth/2, windowHeight*3/4);
   if (this.animation)
     this.animation.frameDelay = 10;
     
     this.sprite.addAnimation("harryRun", this.animation);
+    this.sprite.scale = 0.7;
   
 }
 
--- js/snake.js
+++ js/snake.js
@@ -1,19 +1,29 @@
 class Snake {
 constructor(){
-this.strikeLeft;
-this.strikeRight;
-this.slitherRight = loadAnimation("../images/slither-to-right/sprite_00.png","../images/slither-to-right/sprite_01.png",
-"../images/slither-to-right/sprite_02.png","../images/slither-to-right/sprite_03.png","../images/slither-to-right/sprite_04.png",
-"../images/slither-to-right/sprite_05.png","../images/slither-to-right/sprite_06.png",
-"../images/slither-to-right/sprite_07.png","../images/slither-to-right/sprite_08.png","../images/slither-to-right/sprite_09.png");
-this.slitherLeft = loadAnimation("../images/slither-to-left/sprite_00.png","../images/slither-to-left/sprite_01.png",
-"../images/slither-to-left/sprite_02.png","../images/slither-to-left/sprite_03.png","../images/slither-to-left/sprite_04.png",
-"../images/slither-to-left/sprite_05.png","../images/slither-to-left/sprite_06.png",
-"../images/slither-to-left/sprite_07.png","../images/slither-to-left/sprite_08.png","../images/slither-to-left/sprite_09.png");
+this.strikeRight = loadAnimation("./images/strike-right/sprite_0.png","./images/strike-right/sprite_1.png",
+"./images/strike-right/sprite_2.png","./images/strike-right/sprite_3.png",
+"./images/strike-right/sprite_4.png","./images/strike-right/sprite_5.png");
+this.slitherRight = loadAnimation("./images/slither-to-right/sprite_00.png","./images/slither-to-right/sprite_01.png",
+"./images/slither-to-right/sprite_02.png","./images/slither-to-right/sprite_03.png","./images/slither-to-right/sprite_04.png",
+"./images/slither-to-right/sprite_05.png","./images/slither-to-right/sprite_06.png",
+"./images/slither-to-right/sprite_07.png","./images/slither-to-right/sprite_08.png","./images/slither-to-right/sprite_09.png");
+this.slitherLeft = loadAnimation("./images/slither-to-left/sprite_00.png","./images/slither-to-left/sprite_01.png",
+"./images/slither-to-left/sprite_02.png","./images/slither-to-left/sprite_03.png","./images/slither-to-left/sprite_04.png",
+"./images/slither-to-left/sprite_05.png","./images/slither-to-left/sprite_06.png",
+"./images/slither-to-left/sprite_07.png","./images/slither-to-left/sprite_08.png","./images/slither-to-left/sprite_09.png");
 this.sprite = createSprite(windowWidth/2,windowHeight/2);
-this.sprite.scale = 3;
+this.sprite.scale = 2;
+if(this.slitherRight){
+    this.slitherRight.frameDelay = 7;
+    }
+    if(this.strikeRight){
+        this.strikeRight.frameDelay = 7;
+        }
+
+
 this.sprite.addAnimation("slither-right", this.slitherRight);
 this.sprite.addAnimation("slither-left", this.slitherLeft);
+this.sprite.addAnimation("strike-right", this.strikeRight);
 }
 
 display(){
